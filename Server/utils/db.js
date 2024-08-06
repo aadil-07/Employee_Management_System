ad65cb53bf9d7b8947675b2b608a16c11325fedf
aadil-07 <aadilparwez31122001@gmail.com>
2024-08-06 08:29:43 +0530

    some change in db

--- Server/utils/db.js
+++ Server/utils/db.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql'
-
+const dotenv = require("dotenv");
 const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "employeems"
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 })
 
 con.connect(function(err) {
